a4228080d392bb8b1f489bd213df3e7aafda85f9
Dmitry <spitsynfilmeffect@gmail.com>
2022-11-01 01:47:36 +0300

    its alive

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string[] CreateArray (int Length){
+    var userArray = new string[Length];
+    
+for (int i = 0; i < userArray.Length; i++){
+    System.Console.WriteLine("Введите строку");
+    userArray[i] = Console.ReadLine();
+}
+return userArray;
+}  
+
+void PrintArray(string[] arrayForPrint)
+{
+    for (int i = 0; i < arrayForPrint.Length; i++){
+         System.Console.WriteLine(arrayForPrint[i]);
+    }
+}
+PrintArray(CreateArray (3));
