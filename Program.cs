a57d08e4634e571666de4e34a4d0b7911b4bc6f3
Dmitry <spitsynfilmeffect@gmail.com>
2022-11-01 12:09:43 +0300

    ready for test

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
-﻿string[] CreateArray (int Length){
+﻿ string Prompt(string message)
+{
+    Console.Write(message);
+    string result = Console.ReadLine();
+    return result;
+}
+
+string[] CreateArray (int Length){
     var userArray = new string[Length];
     
 for (int i = 0; i < userArray.Length; i++){
-    System.Console.WriteLine("Введите строку");
+    System.Console.WriteLine($"Введите {i+1}-ю строку");
     userArray[i] = Console.ReadLine();
 }
 return userArray;
@@ -14,4 +21,29 @@ void PrintArray(string[] arrayForPrint)
          System.Console.WriteLine(arrayForPrint[i]);
     }
 }
-PrintArray(CreateArray (3));
+int HowManyStrings(string[] arrayForCheck){
+    int count = 0;
+    for (int i = 0; i < arrayForCheck.Length; i++){
+        if (arrayForCheck[i].Length <= 3){
+            count++;
+        }
+}
+return count;
+}
+
+string[] ReconfigArray(string[] arrayForCheck, int Length){
+    string[] finalArray = new string[Length];
+    int count = 0;
+    for (int i = 0; i < arrayForCheck.Length; i++){
+        if (arrayForCheck[i].Length <= 3){
+            finalArray[count] = arrayForCheck[i];
+            count++;}
+    }
+return finalArray;
+}
+int lengthArray = Convert.ToInt32(Prompt("Введите количество строк > "));
+string[] reqArray = CreateArray(lengthArray);
+int neededNumber =  HowManyStrings(reqArray);
+string[] arrayForPrint = ReconfigArray(reqArray,neededNumber );
+System.Console.WriteLine($"Среди строк, введенных Вами {neededNumber} содержат меньше четырех символов. Вот они:");
+PrintArray(arrayForPrint);
