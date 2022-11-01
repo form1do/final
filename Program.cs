345d8bc480a4f029d80750ba4966d48294c52bd3
Dmitry <spitsynfilmeffect@gmail.com>
2022-11-01 12:41:34 +0300

    readme added, code is done

--- Program.cs
+++ Program.cs
@@ -1,49 +1,63 @@
-﻿ string Prompt(string message)
+﻿string Prompt(string message)
 {
     Console.Write(message);
     string result = Console.ReadLine();
     return result;
 }
 
-string[] CreateArray (int Length){
+string[] CreateArray(int Length)
+{
     var userArray = new string[Length];
-    
-for (int i = 0; i < userArray.Length; i++){
-    System.Console.WriteLine($"Введите {i+1}-ю строку");
-    userArray[i] = Console.ReadLine();
+
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        System.Console.WriteLine($"Введите {i + 1}-ю строку");
+        userArray[i] = Console.ReadLine();
+    }
+    return userArray;
 }
-return userArray;
-}  
 
 void PrintArray(string[] arrayForPrint)
 {
-    for (int i = 0; i < arrayForPrint.Length; i++){
-         System.Console.WriteLine(arrayForPrint[i]);
+    for (int i = 0; i < arrayForPrint.Length; i++)
+    {
+        System.Console.WriteLine(arrayForPrint[i]);
     }
 }
-int HowManyStrings(string[] arrayForCheck){
+int HowManyStrings(string[] arrayForCheck)
+{
     int count = 0;
-    for (int i = 0; i < arrayForCheck.Length; i++){
-        if (arrayForCheck[i].Length <= 3){
+    for (int i = 0; i < arrayForCheck.Length; i++)
+    {
+        if (arrayForCheck[i].Length <= 3)
+        {
             count++;
         }
-}
-return count;
+    }
+    return count;
 }
 
-string[] ReconfigArray(string[] arrayForCheck, int Length){
+string[] ReconfigArray(string[] arrayForCheck, int Length)
+{
     string[] finalArray = new string[Length];
     int count = 0;
-    for (int i = 0; i < arrayForCheck.Length; i++){
-        if (arrayForCheck[i].Length <= 3){
+    for (int i = 0; i < arrayForCheck.Length; i++)
+    {
+        if (arrayForCheck[i].Length <= 3)
+        {
             finalArray[count] = arrayForCheck[i];
-            count++;}
+            count++;
+        }
     }
-return finalArray;
+    return finalArray;
 }
 int lengthArray = Convert.ToInt32(Prompt("Введите количество строк > "));
 string[] reqArray = CreateArray(lengthArray);
-int neededNumber =  HowManyStrings(reqArray);
-string[] arrayForPrint = ReconfigArray(reqArray,neededNumber );
-System.Console.WriteLine($"Среди строк, введенных Вами {neededNumber} содержат меньше четырех символов. Вот они:");
+int neededNumber = HowManyStrings(reqArray);
+string[] arrayForPrint = ReconfigArray(reqArray, neededNumber);
+if (neededNumber > 0) {
+System.Console.WriteLine($"Среди {lengthArray} строк, введенных Вами {neededNumber} содержат меньше четырех символов. Вот они:");
 PrintArray(arrayForPrint);
+}
+else 
+System.Console.WriteLine ($"Среди {lengthArray} строк, введенных Вами нет подходящих заданным условиям") ;
